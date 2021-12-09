382ea487bd12302929ac8abbc7b98c4f0945ea01
Minsoo Kim <minsooerickim@gmail.com>
2021-12-08 18:27:06 -0800

    connected frontend with fastify server using graphql

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -52,24 +52,27 @@ app.register(mercurius, {
     schema,
     resolvers
 })
-
-app.get('/', async function (req, reply) {
+app.register(require('fastify-cors'), {
+  origin: "*",
+  methods: ["GET"]
+})
+app.get('/api/graphql', async function (req, reply) {
     const query = '{ list { title } }'
     return reply.graphql(query)
 })
-app.post('/', async function (req, reply) {
+app.post('/api/graphql', async function (req, reply) {
     console.log("post");
     const mutation = `{ createItem (title: "newItemTitle") { title } }`;
     return reply.graphql(mutation)
 })
-app.put('/', async function (req, reply) {
+app.put('/api/graphql', async function (req, reply) {
   console.log("put");
   const mutation = `{ deleteItem (title: "Item1") { title } }`;
   return reply.graphql(mutation)
 })
 
 
-app.listen(3000)
+app.listen(3001)
 
 //mutation?
 //how does this link with react relay?
