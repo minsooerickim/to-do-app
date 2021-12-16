6d37d8cdd99640590f7859b67cc964193ff3b05d
minsooerickim <minsooerickim@gmail.com>
2021-12-15 20:34:08 -0800

    connected frontend button to db

--- components/button.tsx
+++ components/button.tsx
@@ -1,22 +1,57 @@
+import React from 'react';
 import styles from '../styles/button.module.css'
 
 export default function Button() {
+  const displayTask = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    const container = document.getElementById('tasksContainer');
+    async function fetchTasks() {
+        const response = await fetch('http://localhost:3001/api/graphql');
+        const tasks = await response.json();
+        return tasks;
+    }
+    fetchTasks().then(res => {
+      res.data.list.forEach((task: { title: string; }) => {
+          console.log(task);
+          var p = document.createElement('p')
+          var text = document.createTextNode(task.title);
+          p.appendChild(text);
+          container!.append(p)
+      })
+    });
+  };
+  const createTask = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  }
   return (
       <div className={styles.container}>
         <button className={styles.button}
           title="Create Task"
-          onClick={() => console.log("should create task")}
+          onClick={createTask}
         >
         Create Task
         </button>
+
         <div className={styles.divider}></div>
+        
         <button className={styles.button}
           title="Delete Task"
           onClick={() => console.log("should delete task")}
         >
         Delete Task
         </button>
+
+        <div className={styles.divider}></div>
+
+        <button className={styles.button}
+          title="Display Task"
+          onClick={displayTask}
+        >
+        Display Task
+        </button>
       </div>
 
   )
 }
+
